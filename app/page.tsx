f85b340eecee74558a1555220c5e1471033829db
Jyoti Patowary <61742151+Jyoti-Patowary@users.noreply.github.com>
2024-12-17 18:42:10 +0530

    fixed build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-// import BorrowerJourney from "@/components/BorrowerJourney";
-// import CarouselSection from "@/components/CarouselSection";
+
 import FeaturesSection from "@/components/FeaturesSection";
 import Footer from "@/components/Footer";
 import FooterCards from "@/components/FooterCards";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
-// import StatisticsSection from "@/components/StatisticsSection";
+
 
 
 export default function Home() {
@@ -15,9 +14,6 @@ export default function Home() {
       <main>
         <HeroSection />
         <FeaturesSection />
-        {/* <BorrowerJourney/> */}
-        {/* <CarouselSection />
-        <StatisticsSection /> */}
         <FooterCards/>
       </main>
       <Footer />
