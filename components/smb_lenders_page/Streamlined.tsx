c08972a81ace3168ba7ccc8e4dc35f5de8db0065
Jyoti Patowary <61742151+Jyoti-Patowary@users.noreply.github.com>
2025-01-15 19:40:01 +0530

    updated pages gradient

--- components/smb_lenders_page/Streamlined.tsx
+++ components/smb_lenders_page/Streamlined.tsx
@@ -121,13 +121,54 @@ export default function Streamlined() {
         ))}
         </div>
 
+
+        <div className="block md:hidden relative w-[70%] h-full">
+  <motion.div
+    className="absolute top-0 left-0 w-full bg-gradient-to-tr from-[#66B3B0] to-[#149994] rounded-full h-[350px] sm:w-[400px] sm:h-[450px] md:w-[468.64px] md:h-[542.11px] blur-[125px] opacity-30"
+    initial={{ x: "0%" }}
+    animate={{
+      x: ["-10%", "10%", "-10%", "0%"],
+    }}
+    transition={{
+      duration: 10,
+      repeat: Infinity,
+      ease: "easeInOut",
+    }}
+  />
+
+  <motion.div
+    className="absolute top-0 left-0 w-full bg-gradient-to-tl from-[#1C8DEA] to-[#195BD7] rounded-full h-[250px] sm:w-[500px] sm:h-[600px] md:w-[618.35px] md:h-[633.38px] blur-[125px] opacity-30 -z-10"
+    initial={{ x: "0%" }}
+    animate={{
+      x: ["10%", "-10%", "10%", "0%"],
+    }}
+    transition={{
+      duration: 12,
+      repeat: Infinity,
+      ease: "easeInOut",
+    }}
+  />
+
+  <motion.div
+    className="absolute top-[300px] sm:top-0 left-0 w-full bg-[#BE95FF] rounded-full h-[200px] sm:w-[400px] sm:h-[500px] md:w-[451.48px] md:h-[542.11px] blur-[105px] opacity-30 z-[-1]"
+    initial={{ x: "0%" }}
+    animate={{
+      x: ["-10%", "10%", "-10%", "0%"],
+    }}
+    transition={{
+      duration: 8,
+      repeat: Infinity,
+      ease: "easeInOut",
+    }}
+  />
+</div>
            {/* Mobile Carousel */}
                 <div className="flex md:hidden overflow-x-auto pb-4">
                   <div className="flex flex-nowrap gap-[20px]">
                     {data.cardData.map((card, index) => (
                       <div
                         key={index}
-                        className="relative p-4 backdrop-brightness-110 bg-white/30 rounded-[32px] shadow-md w-[300px] h-auto flex flex-col items-center justify-start transform transition-transform duration-300 hover:scale-105"
+                        className="relative p-4 backdrop-brightness-110 bg-white/60 rounded-[32px] shadow-md w-[300px] h-auto flex flex-col items-center justify-start transform transition-transform duration-300 hover:scale-105"
                       >
                         <div className="mb-4">
                           <Image
