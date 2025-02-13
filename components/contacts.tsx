fd5323685be3a8aa4454c83d3167dcbbcdf7e76a
Samudra Deka <samudradeka@Samudras-MacBook-Air.local>
2025-02-13 11:26:30 +0530

    GAC pages

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -2,6 +2,7 @@
 
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { CustomHeader, CustomSubtitle } from "./CustomHeader";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
@@ -18,7 +19,7 @@ const validationSchema = Yup.object({
 });
 
 
-export default function ContactUs() {
+export default function Contact() {
   const [loading, setLoading] = useState(false);
   const [clicked, setClicked] = useState(false);
   const formik = useFormik({
@@ -30,8 +31,8 @@ export default function ContactUs() {
       message: "",
     },
     validationSchema,
-    onSubmit: async (values: { [key: string]: string }) => {
-      console.log({ values });
+    onSubmit: async (values: {[key:string]:string}) => {
+      console.log({ values })
       setClicked(true);
       setLoading(true);
       const res = await fetch(
@@ -43,25 +44,40 @@ export default function ContactUs() {
           },
           body: JSON.stringify({
             fields: [
-              ...Object.keys(values).map((key: string) => ({
-                name: key,
-                value: values[key],
-              })),
+              ...Object.keys(values).map((key: string) => ({ name: key, value: values[key] }))
             ],
           }),
         }
       );
-      setLoading(false);
+      setLoading(false)
       if (res.ok) {
-        formik.resetForm();
+        formik.resetForm()
       } else {
         alert("Failed to submit form.");
       }
     },
   });
 
-  return <section className="container flex">
-      <div className="bg-[url('/section_images/gac/gac.png')] md:w-[723px] md:h-[861px] object-cover bg-no-repeat bg-cover">ss</div>
+
+
+  return (
+    <section className="container md:w-[1160px] mx-auto mt-32 md:mb-28 md:p-8 
+    flex flex-col md:flex-row font-plus-jakarta justify-center items-start gap-[43px] md:gap-[98px] overflow-hidden">
+      <div className="flex flex-col text-center md:text-left gap-[20px] p-8 md:p-0 relative top-[80px] md:block md:top-0 md:w-[568px]">
+        <CustomHeader
+          className="md:text-[56px] font-bold flex flex-col gap-0"
+          text={
+            <>
+              <span>We’d love to show </span>
+              <span>you around!</span>
+            </>
+          }
+        />
+        <CustomSubtitle
+          text="Whether you’re looking to streamline your loan processes, enhance borrower experiences, or explore our AI-powered, cloud-native solutions, our team is here to help. Connect with us to start your journey toward a smarter, more efficient lending ecosystem."
+        />
+      </div>
+
       <div className="relative w-full max-w-lg pb-[20px] md:pb-0 ">
         <div className="absolute top-[100px] opacity-[30%] -z-10">
           <motion.div
@@ -121,9 +137,7 @@ export default function ContactUs() {
                   className="border border-gray-300 font-plus-jakarta rounded-[8px] p-2 w-full focus:outline-none focus:ring-2 focus:ring-[#1C8DEA]"
                 />
                 {formik.touched.firstname && formik.errors.firstname && (
-                  <p className="text-red-500 font-plus-jakarta text-sm">
-                    {formik.errors.firstname as string}
-                  </p>
+                  <p className="text-red-500 font-plus-jakarta text-sm">{formik.errors.firstname as string}</p>
                 )}
               </div>
               <div className="flex flex-col gap-[12px] w-full">
@@ -140,9 +154,7 @@ export default function ContactUs() {
                   className="border border-gray-300 font-plus-jakarta rounded-[8px] p-2 w-full focus:outline-none focus:ring-2 focus:ring-[#1C8DEA]"
                 />
                 {formik.touched.phone && formik.errors.phone && (
-                  <p className="text-red-500 font-plus-jakarta text-sm">
-                    {formik.errors.phone as string}
-                  </p>
+                  <p className="text-red-500 font-plus-jakarta text-sm">{formik.errors.phone as string}</p>
                 )}
               </div>
             </div>
@@ -161,9 +173,7 @@ export default function ContactUs() {
                   className="border border-gray-300 font-plus-jakarta rounded-[8px] p-2 w-full focus:outline-none focus:ring-2 focus:ring-[#1C8DEA]"
                 />
                 {formik.touched.email && formik.errors.email && (
-                  <p className="text-red-500 font-plus-jakarta text-sm">
-                    {formik.errors.email as string}
-                  </p>
+                  <p className="text-red-500 font-plus-jakarta text-sm">{formik.errors.email as string}</p>
                 )}
               </div>
               <div className="flex flex-col gap-[12px] w-full">
@@ -201,9 +211,7 @@ export default function ContactUs() {
                  p-2 w-full h-[142px] focus:outline-none focus:ring-2 focus:ring-[#1C8DEA]"
               />
               {formik.touched.message && formik.errors.message && (
-                <p className="text-red-500 font-plus-jakarta text-sm">
-                  {formik.errors.message as string}
-                </p>
+                <p className="text-red-500 font-plus-jakarta text-sm">{formik.errors.message as string}</p>
               )}
             </div>
 
@@ -215,34 +223,22 @@ export default function ContactUs() {
               className="bg-[#1C8DEA] flex justify-center items-center w-full font-plus-jakarta
                text-white md:text-[16px] font-bold rounded-[31px] p-4 hover:bg-blue-600 transition"
             >
-              {loading && (
+              {loading &&
+
                 <div role="status">
-                  <svg
-                    aria-hidden="true"
-                    className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
+                  <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                   </svg>
                 </div>
-              )}
-              {!loading
-                ? !clicked
-                  ? "Get in touch"
-                  : "We'll get back to you"
-                : ""}
+
+              }
+              {!loading ? 
+              !clicked ? 'Get in touch' :  "We'll get back to you" :''}
             </motion.button>
           </div>
         </form>
       </div>
     </section>
+  );
 }
