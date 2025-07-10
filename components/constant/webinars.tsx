2c3172a398630ce87f9767c7bc6731d81b7dd876
SamDeka28 <samdeka28@gmail.com>
2025-07-11 01:31:54 +0530

    New changes

---
+++ components/constant/webinars.tsx
@@ -0,0 +1,14 @@
+export const WEBINARS=[
+    {
+        image :"/background_images/heroimg.webp",
+        title : "Generative Al vs. Agentic Al-And Why It Matters for Loan Origination",
+        link: "/webinars/7-aug-2025",
+        eventDate: "7 Aug, 2025"
+    },
+    {
+        image : "/background_images/heroimg.webp",
+        title: "Lending 2030: Beyond AI-Shaping the Future of Lending",
+        link:"/webinars/algebrik-webinar1",
+        eventDate : "7 Feb,2025"
+    }
+]
